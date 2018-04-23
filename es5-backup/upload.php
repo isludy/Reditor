e0adca6f8559e921cc63141216251c87118b2b54
Rudy <670752970@qq.com>
2018-04-24 07:04:35 +0800

    php

--- es5-backup/upload.php
+++ es5-backup/upload.php
@@ -3,15 +3,17 @@ header('Access-Control-Allow-Origin:*');
 //sleep(2);
 if(!empty($_FILES)){
     $r = array('code'=>0,'data'=>array(),'msg'=>'');
+    $root_dir = 'upload/';
+    $root_url = 'http://localhost/demo/upload/';
     foreach ($_FILES as $id => $file){
-        $rename = 'upload/'.$id . strrchr($file['name'], '.');
-        if(!is_dir('upload/')){
+        $rename = $id . strrchr($file['name'], '.');
+        if(!is_dir($root_dir)){
             $r['code'] = 1;
             $r['msg'] = '目录不存在';
             exit(json_encode($r));
         }
-        if(move_uploaded_file($file['tmp_name'], $rename)){
-            $r['data'][$id] = 'http://localhost/demo/'. $rename;
+        if(move_uploaded_file($file['tmp_name'], $root_dir . $rename)){
+            $r['data'][$id] = $root_url . $rename;
         }else{
             $r['code'] = 1;
             $r['msg'] = '临时文件转移失败';
