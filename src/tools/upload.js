7d728ae0869da6a577a00891d5c5f4333c6ec04d
Rudyhub <670752970@qq.com>
2018-07-11 17:35:42 +0800

    up

--- src/tools/upload.js
+++ src/tools/upload.js
@@ -3,31 +3,57 @@ import Up from './upload/Up';
 import Down from './upload/Down';
 import Items from './upload/Items';
 import utils from "../utils";
+import options from "../options";
 
-let box;
-
-export default (reditor)=>{
-    if(!box){
-        box = re(`<div class="re-upload">
-            <div class="re-upload-wrapper">
-                <div class="re-upload-header re-success">文件系统</div>
-                <div class="re-upload-toolbar">
-                    <button id="re-upload-choser" class="re-btn-m re-btn-success">添加</button>
-                    <button id="re-upload-upload" class="re-btn-m re-btn-warning">上传</button>
-                    <button id="re-upload-clear" class="re-btn-m re-btn-danger">清空</button>
-                </div>
-                <div id="re-upload-body" class="re-upload-body"></div>
-                <div class="re-upload-footer">
-                    <button id="re-upload-use" class="re-btn-m re-btn-success">使用</button>
-                    <button id="re-upload-refresh" class="re-btn-m re-btn-warning">刷新</button>
-                </div>
+let context = document.body,
+    box = re(`<div class="re-upload">
+        <div class="re-upload-wrapper">
+            <b class="re-dialog-close">×</b>
+            <div class="re-upload-header re-success">文件系统</div>
+            <div class="re-upload-toolbar">
+                <button id="re-upload-choser" class="re-btn-m re-btn-success">添加</button>
+                <button id="re-upload-upload" class="re-btn-m re-btn-warning">上传</button>
+                <button id="re-upload-clear" class="re-btn-m re-btn-danger">清空</button>
             </div>
-        </div>`);
-        box.on('click',function (e){
-            if(e.target === this) box.toggleClass('re-upload-hide');
-        });
-        document.body.appendChild(box[0]);
+            <div id="re-upload-body" class="re-upload-body"></div>
+            <div class="re-upload-footer">
+                <button id="re-upload-use" class="re-btn-m re-btn-success">使用</button>
+                <button id="re-upload-refresh" class="re-btn-m re-btn-warning">刷新</button>
+            </div>
+        </div>
+    </div>`);
 
+box.on('click',function (e){
+    if(e.target === this || re(e.target).hasClass('re-dialog-close')) {
+        context.removeChild(box[0]);
+    }
+});
+
+
+function loadData(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('get',options.upload.path+'?Reditor=load&date=20180529');//+new Date().format('YMD'));
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200)
+            console.log(xhr.response);
+    };
+    xhr.send();
+}
+// Up.init({
+//     choser: '#re-upload-choser',
+//     upload: '#re-upload-upload',
+//     clear: '#re-upload-clear',
+//     list: '#re-upload-body'
+// });
+// Down.init('#re-upload-body');
+// re('#re-upload-refresh').on('click', function(){
+//     Down.init('#re-upload-body');
+// });
+loadData();
+export default (reditor)=>{
+    context.appendChild(box[0]);
+    // if(!box){
+        /*
         Up.init({
             choser: '#re-upload-choser',
             upload: '#re-upload-upload',
@@ -70,7 +96,8 @@ export default (reditor)=>{
             }
             box.addClass('re-upload-hide');
         });
-    }else{
-        box.toggleClass('re-upload-hide');
-    }
+        */
+    // }else{
+    //     box.toggleClass('re-upload-hide');
+    // }
 }
---
+++ src/tools/upload/Item.js
@@ -0,0 +1,16 @@
+class Item{
+    constructor(){
+        this.id = '';
+        this.thumb = '';
+        this.url = '';
+        this.type = '';
+        this.selected = false;
+        this.logo = '';
+        this.el = document.createElement('div');
+
+        this.el.className = 're-upload-item';
+
+
+    }
+
+}
